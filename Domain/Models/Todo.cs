a490127492a04b0f74e9bb1ab408b78859cef2c2
Parisa Alizadeh <parisaalizadeh13821382@gmail.com>
2025-07-06 11:34:07 -0700

    Add project files.

---
+++ Domain/Models/Todo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public record Todo
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
